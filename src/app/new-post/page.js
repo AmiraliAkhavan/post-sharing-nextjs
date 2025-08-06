c3b3cd1101b8fd2553d129f485f5a185152fb8d1
AmiraliAkhavan <111018923+AmiraliAkhavan@users.noreply.github.com>
2025-08-06 17:13:27 +0330

    useFormAction

--- src/app/new-post/page.js
+++ src/app/new-post/page.js
@@ -1,12 +1,9 @@
-"use client";
-
 import { redirect } from "next/navigation";
 import { storePost } from "@/lib/posts";
-import FormSubmit from "@/components/formSubmit";
-import { useActionState } from "react";
+import PostForm from "@/components/PostForm";
 
 export default function NewPostPage() {
-  async function createPost(formData) {
+  async function createPost(prevState, formData) {
     "use server";
     const title = formData.get("title");
     const image = formData.get("image");
@@ -20,7 +17,7 @@ export default function NewPostPage() {
     if (!content || content.trim().length === 0) {
       errors.push("content is required");
     }
-    if (!image) {
+    if (!image || image.size === 0) {
       errors.push("image is required");
     }
 
@@ -38,33 +35,5 @@ export default function NewPostPage() {
     redirect("/feed");
   }
 
-  const [state, formAction] = useActionState(createPost, {});
-
-  return (
-    <>
-      <h1>Create a new post</h1>
-      <form action={createPost}>
-        <p className="form-control">
-          <label htmlFor="title">Title</label>
-          <input type="text" id="title" name="title" />
-        </p>
-        <p className="form-control">
-          <label htmlFor="image">Image URL</label>
-          <input
-            type="file"
-            accept="image/png, image/jpeg"
-            id="image"
-            name="image"
-          />
-        </p>
-        <p className="form-control">
-          <label htmlFor="content">Content</label>
-          <textarea id="content" name="content" rows="5" />
-        </p>
-        <p className="form-actions">
-          <FormSubmit />
-        </p>
-      </form>
-    </>
-  );
+  return <PostForm action={createPost} />;
 }
